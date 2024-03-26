18afcbcd93419d100c9d05c22afeee1205cf063a
StachuDev <solszakdev@gmail.com>
2024-03-26 12:44:59 +0100

    project template

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+void main() {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Artykuły spożywcze",
+      theme: ThemeData(),
+    );
+  }
+}
