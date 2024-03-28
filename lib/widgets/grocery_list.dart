5f3f5796e8f9625c0d64ab9016b6e58571bdf3d6
StachuDev <solszakdev@gmail.com>
2024-03-28 06:59:47 +0100

    fetching data from firebase and adding them to grocerylist state

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/add_item.dart';
 
@@ -10,20 +14,57 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> groceryItemsList = [];
+  List<GroceryItem> _groceryItemsList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-app-fcec5-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    print(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+            (catItem) => catItem.value.name == item.value['category'],
+          )
+          .value;
+      loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+
+    setState(() {
+      _groceryItemsList = loadedItems;
+    });
+  }
+
   void _addItem() async {
     await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const AddItem(),
       ),
     );
+
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
-    final listIndex = groceryItemsList.indexOf(item);
+    final listIndex = _groceryItemsList.indexOf(item);
 
     setState(() {
-      groceryItemsList.remove(item);
+      _groceryItemsList.remove(item);
     });
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
@@ -34,7 +75,7 @@ class _GroceryListState extends State<GroceryList> {
           label: 'Undo',
           onPressed: () {
             setState(() {
-              groceryItemsList.insert(listIndex, item);
+              _groceryItemsList.insert(listIndex, item);
             });
           },
         ),
@@ -45,30 +86,27 @@ class _GroceryListState extends State<GroceryList> {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
-      itemCount: groceryItemsList.length,
+      itemCount: _groceryItemsList.length,
       itemBuilder: (context, index) => Dismissible(
-        key: ValueKey(groceryItemsList[index].id),
-        background: Container(
-          color: Theme.of(context).colorScheme.error.withOpacity(0.8),
-        ),
-        onDismissed: (direction) => _removeItem(groceryItemsList[index]),
+        key: ValueKey(_groceryItemsList[index].id),
+        onDismissed: (direction) => _removeItem(_groceryItemsList[index]),
         child: ListTile(
           leading: Container(
             width: 20,
             height: 20,
-            color: groceryItemsList[index].category.color,
+            color: _groceryItemsList[index].category.color,
           ),
           title: Text(
-            groceryItemsList[index].name,
+            _groceryItemsList[index].name,
           ),
           trailing: Text(
-            groceryItemsList[index].quantity.toString(),
+            _groceryItemsList[index].quantity.toString(),
           ),
         ),
       ),
     );
 
-    if (groceryItemsList.isEmpty) {
+    if (_groceryItemsList.isEmpty) {
       content = const Center(
         child: Text('Nie zapisano żadnych produktów'),
       );
